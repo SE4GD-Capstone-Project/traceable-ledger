3649e1716bdddaa098cd4f69275ad3d9c97bcfb6
Quazi Ghulam Rafi <quazirafi1@gmail.com>
2024-03-07 09:27:56 +0200

    dockerized, need to rework on the DBConnection for the container

--- NodeAPI/urls.py
+++ NodeAPI/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet
-
+from NodeAPI import views
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 
 urlpatterns = [
+    path('products/', views.demo_get),
     path('', include(router.urls)),
 ]
--- NodeAPI/views.py
+++ NodeAPI/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+
+def demo_get(request):
+    static_data = {
+        "key1" : "val1",
+        "key2" : "val2",
+        "list" : [1,2,3] 
+    }
+    return JsonResponse(static_data)
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
