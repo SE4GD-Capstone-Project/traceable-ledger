3b0515f87b27f5bcab1aee9f4fa2de6f48f58ef3
Mhreteabe <mhretebetesfaye71624@gmail.com>
2024-04-09 07:38:36 +0300

    reverted to previous version

--- NodeAPI/views.py
+++ NodeAPI/views.py
@@ -4,13 +4,6 @@ from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
 
-def demo_get(request):
-    static_data = {
-        "key1" : "val1",
-        "key2" : "val2",
-        "list" : [1,2,3] 
-    }
-    return JsonResponse(static_data)
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
