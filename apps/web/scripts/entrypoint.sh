f2460746ba68483b0e404b86b65ac6beea8b8a5c
Quazi Ghulam Rafi <quazirafi1@gmail.com>
2024-05-04 14:14:39 +0300

    migrated to postgres and dockerized

--- apps/web/scripts/entrypoint.sh
+++ apps/web/scripts/entrypoint.sh
@@ -3,3 +3,19 @@
 APP_PORT=${PORT:-8000}
 
 /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm backend.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
